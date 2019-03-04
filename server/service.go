aafbc185a2b84d10b91041e866333dd9c5b6d826
Prashanth Pai <prashanth.pai@plivo.com>
2019-03-04 09:27:16 +0530

    Add phone number sanitization
    Signed-off-by: Prashanth Pai <prashanth.pai@plivo.com>

--- server/service.go
+++ server/service.go
@@ -2,10 +2,14 @@ package server
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/ppai-plivo/mnlookup/api"
+
+	"github.com/nyaruka/phonenumbers"
 )
 
 type LookupService interface {
@@ -16,6 +20,24 @@ type Service struct {
 	l LookupService
 }
 
+func (s *Service) santize(number string) (string, error) {
+
+	replacer := strings.NewReplacer(" ", "", "-", "", "(", "", ")", "", "+", "", "/", "")
+	number = replacer.Replace(strings.TrimLeft(number, "0"))
+
+	pNum, err := phonenumbers.Parse("+"+number, "")
+	if err != nil {
+		return "", err
+	}
+
+	if !phonenumbers.IsValidNumber(pNum) {
+		return "", fmt.Errorf("Invalid phone number")
+	}
+
+	fNum := phonenumbers.Format(pNum, phonenumbers.E164)
+	return fNum[1:], nil
+}
+
 func (s *Service) Handler(w http.ResponseWriter, r *http.Request) {
 
 	var number string
@@ -24,6 +46,13 @@ func (s *Service) Handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if number == "" {
+		http.NotFound(w, r)
+	}
+
+	var err error
+	number, err = s.santize(number)
+	if err != nil {
+		log.Println(err)
 		http.NotFound(w, r)
 	}
 
